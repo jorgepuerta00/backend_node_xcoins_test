5367e054095b8aae023272ec8b70b3c65c68fd0d
Jorge Puerta <jorge.puerta00@gmail.com>
2021-11-14 22:11:12 -0500

    simulator router updated

--- src/controllers/SimulatorController.ts
+++ src/controllers/SimulatorController.ts
@@ -7,24 +7,24 @@ import { GetSimulatorByProfileId } from "../usesCases/Simulator/GetSimulatorByPr
 
 export class SimulatorController {
 
-    public _SimulatorRepository: SimulatorRepository;
-    public _mapEntity: SimulatorEntityMap;
+  public _SimulatorRepository: SimulatorRepository;
+  public _mapEntity: SimulatorEntityMap;
 
-    constructor() {
-        this._SimulatorRepository = new SimulatorRepository();
-        this._mapEntity = new SimulatorEntityMap();
-    }
+  constructor() {
+    this._SimulatorRepository = new SimulatorRepository();
+    this._mapEntity = new SimulatorEntityMap();
+  }
 
-    async get(req, res) {
-        const useCase = new GetAllSimulators(this._SimulatorRepository,this._mapEntity);
-        const Simulator = await useCase.execute();
-        return res.json({ Simulator });
-    }
+  async get(req, res) {
+    const useCase = new GetAllSimulators(this._SimulatorRepository,this._mapEntity);
+    const Simulator = await useCase.execute();
+    return res.json({ Simulator });
+  }
 
-    async getByProfileId(req, res) {
-        const useCase = new GetSimulatorByProfileId(this._SimulatorRepository, this._mapEntity);
-        useCase.setProfileId(req.params.profile_id);
-        const data = await useCase.execute();
-        res.json(data);
-    }
+  async getByProfileId(req, res) {
+    const useCase = new GetSimulatorByProfileId(this._SimulatorRepository, this._mapEntity);
+    useCase.setProfileId(req.params.profile_id);
+    const data = await useCase.execute();
+    res.json(data);
+  }
 }
